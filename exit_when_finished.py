f264b676f6abc49d38b72eea15e0da241f8178c6
Qiu <35896157+cy745@users.noreply.github.com>
2022-12-27 01:47:00 +0800

    引入Random-prompt的代码

--- exit_when_finished.py
+++ exit_when_finished.py
@@ -1,18 +1,24 @@
 import modules.scripts as scripts
 import gradio as gr
 import os
+import re
+import random
+
+import modules.shared as shared
+import modules.sd_samplers
 
 from modules import images
-from modules.processing import process_images, Processed
-from modules.processing import Processed
+from modules.processing import process_images, Processed, StableDiffusionProcessingTxt2Img
 from modules.shared import opts, cmd_opts, state
+
+
 
 
 class Script(scripts.Script):  
 
     # The title of the script. This is what will be displayed in the dropdown menu.
     def title(self):
-        return "Exit When Finished!"
+        return "Exit When Finished! With Random-Prompt"
 
     # Determines when the script should be shown in the dropdown menu via the 
     # returned value. As an example:
@@ -26,20 +32,98 @@ class Script(scripts.Script):
     # Most UI components can return a value, such as a boolean for a checkbox.
     # The returned values are passed to the run method as parameters.
     def ui(self, is_img2img):
-        enable = gr.Checkbox(False, label="Enable exit function when process is finished.")
-        return [enable]
+        enable = gr.Checkbox(value=False, label="[Exit-When-Finished] Enable exit function when process is finished.")
+        random_prompt_enable = gr.Checkbox(value=False, label="[Random-Prompt] Enable random prompt script function.")
+        use_same_seed = gr.Checkbox(value=False, label="[Random-Prompt] Use same seed to generate.")
+        return [enable, random_prompt_enable, use_same_seed]
   
-    # This is where the additional processing is implemented. The parameters include
-    # self, the model object "p" (a StableDiffusionProcessing class, see
-    # processing.py), and the parameters returned by the ui method.
-    # Custom functions can be defined here, and additional libraries can be imported 
-    # to be used in processing. The return value should be a Processed object, which is
-    # what is returned by the process_images method.
-    def run(self, p, enable):
+
+    def random_prompt_run(self, p, sameseed):
+        original_prompt = p.prompt[0] if type(p.prompt) == list else p.prompt
+
+        all_prompts = [original_prompt]
+        
+
+        split_str=['']
+        right_str=[]
+        gen_prompt=''
+        new_prompt=''
+        
+        for this_prompt in all_prompts:
+            for data in re.finditer(r'(<([^>]+)>)', this_prompt):
+                if data:
+                    span = data.span(1)
+                    new_prompt = this_prompt[:span[0]]
+                    gen_prompt = this_prompt[span[0]:]
+                    this_str=gen_prompt.replace("<",">")
+                    split_str =this_str.split(">")
+                    break
+        
+        #print(f"new_prompt：{new_prompt}")
+        #print(f"gen_prompt：{gen_prompt}")
+        
+        
+        split_str[0]=split_str[0].strip()
+        split_str[0]=split_str[0].strip(',')
+        split_str[0]=split_str[0]+' '
+        for in_str in split_str:
+            if "|" not in in_str:
+                if "," in in_str:
+                    FL_str=in_str.split(",")
+                    right_str.append(FL_str[0])
+                    right_str.append(FL_str[1])
+                else:
+                    right_str.append(in_str)
+        #print (right_str)
+        #print (len(right_str))
+        #d=int((len(right_str)-1)/2)
+        #print(d)
+        
+        for ip in range(p.n_iter+1):
+            
+            my_prompt=""
+            
+            i=0
+            for data in re.finditer(r'(<([^>]+)>)', gen_prompt):
+                if data:
+
+                    items = data.group(2).split("|")#=list
+                    length=len(items)-1
+
+                    rand_item = items[random.randint(0,length)]
+                    
+                    rand_str=right_str[i]+rand_item+right_str[i+1]
+                    
+                    my_prompt=my_prompt+rand_str.strip()+","
+                    i=i+2
+                    
+            i_prompt=new_prompt+my_prompt
+            all_prompts.append(i_prompt)
+            #print(f"i_prompt：{i_prompt}")
+            #print(f"all_prompts len：{len(all_prompts)}")
+            
+        if gen_prompt != '':
+            all_prompts.remove(all_prompts[0])
+        
+
+        p.prompt = all_prompts * p.n_iter #all_prompts * p.n_iter
+        if sameseed == True:
+            p.seed =[item for item in range(int(p.seed), int(p.seed) + p.n_iter) for _ in range(len(all_prompts))]
+        #print(f"p.n_iter：{p.n_iter}") #=batch_count
+        p.do_not_save_grid = True
+        p.prompt_for_display = original_prompt
+
+        return process_images(p)
+
+
+    def run(self, p, enable, random_prompt_enable, use_same_seed):
         if enable:
             print("[Exit when done]Script is enable. \n")
 
-        proc = process_images(p)
+        if random_prompt_enable:
+            print("[Random-Prompt]Script is enable. \n")
+
+        proc = random_prompt_run(self, p, use_same_seed) if random_prompt_enable else process_images(p)
 
         if enable:
             print("[Exit when done]try exit.\n")
